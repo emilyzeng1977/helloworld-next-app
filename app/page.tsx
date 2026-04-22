50b7267682104edda73aa3cebc4b693b6bc2256b
tom <tniu@infomedia.com>
2026-04-22 23:02:59 +1000

    init

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import pkg from '../package.json';
 export default function Home() {
     return (
         <main style={{ textAlign: 'center', marginTop: '100px' }}>
-            <h1>👋 HelloWork</h1>
+            <h1>👋 HelloWork 1</h1>
             <p>Welcome to my Next.js app deployed on Vercel</p>
             <p>Version: {pkg.version}</p>
         </main>
