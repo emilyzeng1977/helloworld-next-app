636c4431f1e60a31b9e17f461357795d54b66c62
tom <tniu@infomedia.com>
2026-04-22 18:38:33 +1000

    init

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <main style={{ textAlign: 'center', marginTop: '100px' }}>
+        <h1>👋 HelloWork</h1>
+        <p>Welcome to my Next.js app deployed on Vercel</p>
+      </main>
+  );
+}
