f9bbb1d72d860fd39c9b6304675927d089da063c
emily <tom.niu8@gmail.com>
2026-04-24 11:36:48 +1000

    modify hellowork 2 to hellowork 3

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import pkg from '../package.json';
 export default function Home() {
     return (
         <main style={{ textAlign: 'center', marginTop: '100px' }}>
-            <h1>👋 HelloWork 2</h1>
+            <h1>👋 HelloWork 3</h1>
             <p>Welcome to my Next.js app deployed on Vercel</p>
             <p>Version: {pkg.version}</p>
         </main>
